0b879b3889e7d29d641a8c894925017bcbbe167c
burningborn <evgenyborn@gmail.com>
2021-07-07 17:14:31 +0400

    HomeWork

--- Inheritance/Figures/main.cpp
+++ Inheritance/Figures/main.cpp
@@ -1,10 +1,12 @@
 ﻿#define _USE_MATH_DEFINES
 #include<iostream>
 #include<Windows.h>
-
+#include<wingdi.h>
+using namespace std;
 using std::cin;
 using std::cout;
 using std::endl;
+
 
 namespace Geometry
 {
@@ -31,9 +33,10 @@ namespace Geometry
 	{
 	protected:
 		Color color;
-
+		UINT start_x;
+		UINT start_y;
 	public:
-		Shape(Color color) :color(color) {}
+		Shape(Color color, UINT start_x = 0, UINT start_y = 0 ) :color(color), start_x(start_x), start_y(start_y) {}
 		virtual	~Shape() {}
 
 		virtual double get_area()const = 0;
@@ -53,7 +56,17 @@ namespace Geometry
 			this->side = side;
 			return this->side;
 		}
-		Square(Color color, double side) :Shape(color)
+		void set_start_x(UINT x)
+		{
+			if (x > 1000)x = 1000;
+			start_x = x;
+		}
+		void set_start_y(UINT y)
+		{
+			if (y > 700)y = 700;
+			start_y = y;
+		}
+		Square(Color color, UINT start_x, UINT start_Y,  double side) :Shape(color, start_x, start_y )
 		{
 			set_side(side);
 		}
@@ -70,19 +83,17 @@ namespace Geometry
 		}
 		void drow()const
 		{
-			HANDLE hConsole = GetStdHandle(STD_OUTPUT_HANDLE);
-			unsigned int default_color;
-			SetConsoleTextAttribute(hConsole, color);
+			HWND hwnd = GetConsoleWindow();
+			HDC hdc = GetDC(hwnd);
 
-			for (int i = 0; i < side; i++)
-			{
-				for (int j = 0; j < side; j++)
-				{
-					cout << "* ";
-				}
-				cout << endl;
-			}
-			SetConsoleTextAttribute(hConsole, 7);
+			HPEN hPen = CreatePen(PS_SOLID, 50, color);
+			HBRUSH hBrush = CreateSolidBrush(color);
+
+			SelectObject(hdc, hPen);
+			Rectangle(hdc, start_x, start_y, start_x + side, start_y + side);			
+
+			DeleteObject(hPen);
+			ReleaseDC(hwnd, hdc);
 		}
 		void info()const
 		{
@@ -96,7 +107,7 @@ namespace Geometry
 		}
 	};
 
-	class Rectangle :public Shape
+	class Rectangles :public Shape
 	{
 		double lenght;
 		double width;
@@ -114,12 +125,12 @@ namespace Geometry
 			if (width <= 0)width = 1;
 			this->width = width;
 		}
-		Rectangle(Color color, double lenght, double width) :Shape(color)
+		Rectangles(Color color, UINT start_x, UINT start_y, double lenght, double width) :Shape(color, start_x, start_y)
 		{
 			set_lenght(lenght);
 			set_widht(width);
 		}
-		~Rectangle() {}
+		~Rectangles() {}
 
 		double get_area()const
 		{
@@ -131,19 +142,20 @@ namespace Geometry
 		}
 		void drow()const
 		{
-			HANDLE hConsole = GetStdHandle(STD_OUTPUT_HANDLE);
-			unsigned int default_color;
-			SetConsoleTextAttribute(hConsole, color);
+			HWND hwnd = GetConsoleWindow();
+			HDC hdc = GetDC(hwnd);
 
-			for (int i = 0; i < width; i++)
-			{
-				for (int j = 0; j < lenght; j++)
-				{
-					cout << "* ";
-				}
-				cout << endl;
-			}
-			SetConsoleTextAttribute(hConsole, 7);
+			HPEN hPen = CreatePen(PS_SOLID, 20, color);
+			HBRUSH hBrush = CreateSolidBrush(color);
+
+			SelectObject(hdc, hPen);
+	
+		    Rectangle(hdc, start_x, start_y, start_x + lenght, start_y + width);
+			
+			DeleteObject(hPen);
+			//DeleteObject(hBrush);
+			ReleaseDC(hwnd, hdc);
+
 		}
 		void info()const
 		{
@@ -157,10 +169,9 @@ namespace Geometry
 			drow();
 		}
 	};
+
 	class Circle :public Shape
-	{
-		UINT start_x;
-		UINT start_y;
+	{		
 		double radius;
 	public:
 		double get_radius()const { return radius; }
@@ -180,7 +191,7 @@ namespace Geometry
 			if (y >= 700)y = 700;
 			start_y = y;
 		}
-		Circle(Color color, double radius, UINT start_x = 0, UINT start_y = 0) :Shape(color)
+		Circle(Color color, UINT start_x, UINT start_y, double radius) :Shape(color, start_x, start_y)
 		{
 			set_radius(radius);
 			set_start_x(start_x);
@@ -240,19 +251,17 @@ namespace Geometry
 	class Triangle :public Shape
 		{
 		public:
-			Triangle(Color color) :Shape(color) {}
+			Triangle(Color color) :Shape(color, start_x, start_y) {}
 			~Triangle() {}
 
 	};
 
 	class EquilateralTriangle : Triangle
 	{
-		UINT start_x;
-		UINT start_y;
 			double side;
 		public:
 			double get_side()const { return side; }
-			double get_hight()const
+			double get_height()const
 			{
 				return side * pow(3, .5) / 2;
 			}
@@ -271,22 +280,24 @@ namespace Geometry
 				if (y >= 700)y = 700;
 				start_y = y;
 			}
-			EquilateralTriangle(Color color, double side, UINT start_x=0, UINT start_y=0) :Triangle(color)
+
+			EquilateralTriangle(Color color, UINT start_x, UINT start_y, double side) :Triangle(color)
 			{
 				set_side(side);
 				set_start_x(start_x);
 				set_start_y(start_y);
 			}
 			~EquilateralTriangle(){}
+
 			double get_area()const
 			{
-				return side * 3;
+				return side * side * pow(3, 0.5) / 4;
 			}
-
 			double get_perimeter()const
 			{
-				return side * 3;
+				return side * 3;  
 			}
+
 			void drow()const
 			{
 				HWND hwnd = GetConsoleWindow();
@@ -301,7 +312,7 @@ namespace Geometry
 				{
 					{start_x, start_y + side},
 				    {start_x + side, start_y + side},
-					{start_x + side / 2, start_y + side - get_hight()}
+					{start_x + side / 2, start_y + side - get_height()}
 				};
 
 				Polygon(hdc, points, sizeof(points) / sizeof(POINT));
@@ -311,13 +322,12 @@ namespace Geometry
 
 				ReleaseDC(hwnd, hdc);
 			}
-
 			void info()const
 			{
 				cout << endl;
-				cout << "Треугольник\n";
+				cout << "Равносторонний треугольник\n";
 				cout << "Дина стороны: " << side << endl;
-				cout << "Высота      : " << side << endl;
+				cout << "Высота      : " << get_height() << endl;
 				cout << "Площадь: " << get_area() << endl;
 				cout << "Периметр: " << get_perimeter() << endl;
 				cout << endl;
@@ -325,7 +335,165 @@ namespace Geometry
 			}
 
 	};
-	//class IsoscelesTriange
+
+	class IsoscelesTriange : Triangle
+	{
+		double side;
+	public:
+		double get_side()const { return side; }
+		double get_height()const{return side / 4 * 3.7;}
+		double get_baseside()const { return sqrt(side * side - get_height() * get_height()) * 2; }
+		void set_side(double side)
+		{
+			if (side <= 0)side = 1;
+			this->side = side;
+		}
+		void set_start_x(UINT x) 
+		{ 
+			if (x > 1000)x = 1000;
+			start_x = x;
+		}
+		void set_start_y(UINT y) 
+		{ 
+			if (y > 700)y = 700;
+			start_y = y;
+		}
+
+		IsoscelesTriange(Color color, UINT start_x, UINT start_y, double side) :Triangle(color)
+		{
+			set_side(side);
+			set_start_x(start_x);
+			set_start_y(start_y);
+		}
+		~IsoscelesTriange() {};
+
+		double get_area()const {return (get_baseside() * get_height()) / 2;}
+		double get_perimeter()const { return side * 2 + get_baseside(); }
+
+		void drow()const
+		{
+			cout<< "Равнобедренный треугольник" << endl;
+
+			HWND hwnd = GetConsoleWindow();
+			HDC hdc = GetDC(hwnd);
+
+			HBRUSH hBrash = CreateSolidBrush(color);
+			HPEN hPen = CreatePen(PS_SOLID, 5, color);
+
+			SelectObject(hdc, hPen);
+			//SelectObject
+			POINT points[] =
+			{
+				{start_x, start_y + side},
+				{start_x + get_baseside(), start_y + side},
+				{start_x + get_baseside() / 2,start_y + side - get_height()}
+
+			};
+
+			Polygon(hdc, points, sizeof(points) / sizeof(POINT));
+			
+
+			DeleteObject(hPen);
+			DeleteObject(hBrash);
+			ReleaseDC(hwnd, hdc);
+		}
+
+		void info()const
+		{
+			cout << endl;
+			cout << "Равнобедренный треугольник" << endl;
+			cout << "Длина боковой стороны: " << get_side() << endl;
+			cout << "Высота: (3/4 длины бедра)" << get_height() << endl;
+			cout << "Площадь: " << get_area() << endl;
+			cout << "Периметр: " << get_perimeter() << endl;
+			cout << endl;
+			drow();
+		}
+	};
+
+	class OrthogonalTriangle : Triangle
+	{
+		double leg_a;
+		double leg_b;
+	public:
+		double get_leg_a()const { return leg_a; }
+		double get_leg_b()const { return leg_b; }
+		double get_hypotenuse()const { return sqrt(leg_a* leg_a+ leg_b* leg_b); }
+
+		void set_leg_a(double leg_a)
+		{
+			if (leg_a <= 0)leg_a = 1;
+			this->leg_a = leg_a;
+		}
+		void set_leg_b(double leg_b)
+		{
+			if (leg_b <= 0)leg_b = 1;
+			this->leg_b = leg_b;
+		}
+
+		void set_start_x(UINT x)
+		{
+			if (x > 1000)x = 1000;
+			start_x = x;
+		}
+		void set_start_y(UINT y)
+		{
+			if (y > 700)y = 700;
+			start_y = y;
+		}
+
+		OrthogonalTriangle(Color color, UINT start_x, UINT start_y, double leg_a, double leg_b) :Triangle(color)
+		{
+			set_leg_a(leg_a);
+			set_leg_b(leg_b);
+			set_start_x(start_x);
+			set_start_y(start_y);
+		}
+		~OrthogonalTriangle() {};
+
+		double get_area()const { return (leg_a * leg_b) / 2; }
+		double get_perimeter()const { return leg_a + leg_b + get_hypotenuse(); }
+
+		void drow()const
+		{
+			cout << "ПРямоугольный треугольник" << endl;
+
+			HWND hwnd = GetConsoleWindow();
+			HDC hdc = GetDC(hwnd);
+
+			HBRUSH hBrash = CreateSolidBrush(color);
+			HPEN hPen = CreatePen(PS_SOLID, 5, color);
+
+			SelectObject(hdc, hPen);
+			//SelectObject
+			POINT points[] =
+			{
+				{start_x, start_y + leg_a},
+				{start_x + leg_b, start_y + leg_a},
+				{start_x,start_y}
+
+			};
+
+			Polygon(hdc, points, sizeof(points) / sizeof(POINT));
+
+			DeleteObject(hPen);
+			DeleteObject(hBrash);
+			ReleaseDC(hwnd, hdc);
+		}
+
+		void info()const
+		{
+			cout << endl;
+			cout << "Прямоугольный треугольник" << endl;
+			cout << "Катет A: " << get_leg_a() << endl;
+			cout << "Катет B: " << get_leg_b() << endl;
+			cout << "Гипотенуза: " << get_hypotenuse() << endl;
+			cout << "Площадь: " << get_area() << endl;
+			cout << "Периметр: " << get_perimeter() << endl;
+			cout << endl;
+			drow();
+		}
+	};
 }
 
 //Rectangle(HDC hdc, UINT left_up_x, UNIT left_up_y, UNIT right _down_x, UNIT right _down_y);
@@ -339,15 +507,22 @@ void main()
 	COORD buffer = { 80,50 };
 	SetConsoleDisplayMode(hConsole, CONSOLE_FULLSCREEN, &buffer);
 
-	Geometry::Square square(Geometry::Color::console_red, 5);
+	Geometry::Square square(Geometry::Color::red, 200, 600, 200);
 	square.info();
 
-	Geometry::Rectangle rectangle(Geometry::Color::console_green, 4, 8);
+	Geometry::Rectangles rectangle(Geometry::Color::blue, 100, 350, 300, 400);
 	rectangle.info();
 
 	Geometry::Circle circle(Geometry::Color::red, 100, 880, 200);
 	circle.info();
 
-	Geometry::EquilateralTriangle equil_triangle(Geometry::Color::red, 200, 200, 200);
+	Geometry::EquilateralTriangle equil_triangle(Geometry::Color::red,  250, 400, 400);
 	equil_triangle.info();
+
+	Geometry::IsoscelesTriange iso_triangle(Geometry::Color::red, 100, 300, 300);
+	iso_triangle.info();
+	Geometry::OrthogonalTriangle Ort_Triangle(Geometry::Color::red, 100, 300, 300, 200);
+	Ort_Triangle.info();
+
+	//BOOL Rectangle
 }
